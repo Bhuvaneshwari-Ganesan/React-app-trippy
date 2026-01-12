2971e7905443ae85b8d15e5e778ceacf37701a78
Bhuvaneshwari <162098104+Bhuvaneshwari-Ganesan@users.noreply.github.com>
2026-01-12 11:27:40 +0000

    commit all the change

--- src/routes/About.js
+++ src/routes/About.js
@@ -2,12 +2,13 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
 import AboutUs from "../components/AboutUs";
+import nightImg from "../assets/night.jpg";
 
 function About() {
   return (
     <>
       <Navbar />
-      <Hero cName="hero-mid" heroImg="/assets/night.jpg" title="About" />
+      <Hero cName="hero-mid" heroImg={nightImg} title="About" />
       <AboutUs />
       <Footer />
     </>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,13 +3,15 @@ import Hero from "../components/Hero";
 import Destination from "../components/Destination";
 import Trip from "../components/Trip";
 import Footer from "../components/Footer";
+import twelveImg from "../assets/twelve.jpg";
+
 function Home() {
   return (
     <>
       <Navbar />
       <Hero
         cName="hero"
-        heroImg="/assets/twelve.jpg"
+        heroImg={twelveImg}
         title="Your Journey Your Story"
         text="Choose your Favourite Destination."
         buttonText="Travel Plan"
